17ebb189c2e121ef0bf65e18aec4e5158fae39f5
Ivan Intyakov <18148107+Ivan-Intyakov@users.noreply.github.com>
2020-08-05 03:25:30 +0300

    Updates

---
+++ precache-manifest.48b174edd9e5b11dcb6e78b9feb516c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55b1ab3cd1c3292c19cdedae89879cc",
+    "url": "/ISuMin/robofriends/index.html"
+  },
+  {
+    "revision": "1594426c17b8fec1dda1",
+    "url": "/ISuMin/robofriends/static/css/main.73bf3362.chunk.css"
+  },
+  {
+    "revision": "e13aa5aa07d8cdbcc425",
+    "url": "/ISuMin/robofriends/static/js/2.5e776654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISuMin/robofriends/static/js/2.5e776654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1594426c17b8fec1dda1",
+    "url": "/ISuMin/robofriends/static/js/main.7ebeaea1.chunk.js"
+  },
+  {
+    "revision": "2953a6e297b7b8c0a883",
+    "url": "/ISuMin/robofriends/static/js/runtime-main.27788943.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ISuMin/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
